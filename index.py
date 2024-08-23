7fee71de544f0972675b83a6f95fca213ddbb841
bruna-0307 <bruna1cunha2@gmail.com>
2024-08-23 12:13:38 -0300

    adicionei a função de multiplicação

---
+++ index.py
@@ -0,0 +1,12 @@
+def soma(a,b):
+    return a + b 
+
+def subtracao(a,b):
+    return a - b
+
+    if name=="main":
+            print(soma(10,5))
+            print(subtracao(10,5))
+
+def multiplicacao(a,b):
+    return a*b 
